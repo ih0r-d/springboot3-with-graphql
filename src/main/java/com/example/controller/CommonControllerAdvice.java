59761bfcd0679cd98dc960020006809018c40374
Ihor Didyk <ihordidyk95@gmail.com>
2024-02-01 14:53:18 +0200

    cleanup code, add custom task for build

--- src/main/java/com/example/controller/CommonControllerAdvice.java
+++ src/main/java/com/example/controller/CommonControllerAdvice.java
@@ -15,5 +15,4 @@ public class CommonControllerAdvice {
                 .build();
     }
 
-
 }
--- src/main/java/com/example/exceptions/ApplianceNotFoundException.java
+++ src/main/java/com/example/exceptions/ApplianceNotFoundException.java
@@ -1,11 +1,5 @@
 package com.example.exceptions;
 
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-//@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ApplianceNotFoundException extends RuntimeException{
     public ApplianceNotFoundException(String message) {
         super(message);
